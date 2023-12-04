de3e0f69487a1e7459a9f946154e4a718fac5d04
Yuri Marques <140259786+yuri-marrques@users.noreply.github.com>
2023-12-04 20:48:51 -0300

    Add files via upload

---
+++ js/routes.js
@@ -0,0 +1,29 @@
+export class Router{
+    routes = {}
+
+    add(routName, page){
+        this.routes[routName] = page
+    }
+
+    route (event){
+        event = event || window.event
+        event.preventDefault()
+
+        window.history.pushState({}, '', event.target.href)
+        console.log(event.target.href)
+
+        this.handle()
+    }
+
+    handle(){
+        const {pathname} = window.location
+        const route = this.routes[pathname] || this.routes[404]
+
+        fetch(route)
+        .then(data => data.text())
+        .then(html =>{
+            document.getElementById('app').innerHTML = html
+        })
+
+    }
+}
